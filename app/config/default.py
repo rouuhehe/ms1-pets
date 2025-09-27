6a0aef1c197929e4df063377d13ac3b0d256ba14
rouuhehe <rojasevelyn88@gmail.com>
2025-09-27 11:14:20 -0500

    refactor settings class

--- app/config/default.py
+++ app/config/default.py
@@ -5,28 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    DEBUG: bool = False
-    TESTING: bool = False
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-    ITEMS_PER_PAGE: int = 20
-    LOG_LEVEL: str = os.getenv("LOG_LEVEL")
-    APP_ENV: str = "default"
-    '''
-    S3_BASE_URL: str = os.getenv("S3_BASE_URL")
-    S3_BUCKET: str = os.getenv("S3_BUCKET")
-    AWS_ACCESS_KEY_ID: str = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY: str = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_SESSION_TOKEN: str = os.getenv("AWS_SESSION_TOKEN")
-    AWS_DEFAULT_REGION: str = os.getenv("AWS_DEFAULT_REGION")
-    '''
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
 
+    class Config:
+        extra = "ignore"
 
 settings = Settings()
