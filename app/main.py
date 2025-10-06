2f87ee7ad8f0906ea3df12dade0fb92910e57f3b
g0nzalink <gonzalink1907@gmail.com>
2025-10-05 23:53:21 -0500

    fix: cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from .db import Base, engine
 from .routes import pets, center, adoption_status, vaccines
+from fastapi.middleware.cors import CORSMiddleware
 
 # Inicializar tablas
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="MS1 Pets")
+
+origins = [
+    "https://main.d28p0502xzwadt.amplifyapp.com",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(pets.router)
 app.include_router(center.router)
